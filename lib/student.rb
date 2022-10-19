f8ed79c0995de46f50bbeb72c742afd0e6f4e1c3
Paul Yang <paulyang00@gmail.com>
2022-10-18 23:13:09 -0400

    commit tree 0246ac5c527d6228fee5c938d81c97116c87c710

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+class Student < User
+    attr_accessor :knowledge 
+    
+    def initialize
+        @knowledge = []
+    end
 
+    def learn(knowledge)
+        @knowledge << knowledge # 6 & 7
+    end
+    
+    def knowledge #7
+        @knowledge
+    end
 end
+
